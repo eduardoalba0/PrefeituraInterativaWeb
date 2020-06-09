13161d0c920b69d1562dcc33e3bbe1f640e37237
eduardoalba0 <eduardoalba0@hotmail.com>
2020-06-09 17:54:59 -0300

    Padronização das mensagens.

--- src/main/java/br/edu/ifpr/bsi/prefeiturainterativaweb/bean/AbstractBean.java
+++ src/main/java/br/edu/ifpr/bsi/prefeiturainterativaweb/bean/AbstractBean.java
@@ -1,7 +1,6 @@
 package br.edu.ifpr.bsi.prefeiturainterativaweb.bean;
 
 import java.io.Serializable;
-import java.util.concurrent.Executor;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -9,8 +8,6 @@ import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 
 import org.primefaces.PrimeFaces;
-
-import com.google.common.util.concurrent.MoreExecutors;
 
 @SuppressWarnings("serial")
 public abstract class AbstractBean implements Serializable {
@@ -20,30 +17,50 @@ public abstract class AbstractBean implements Serializable {
 
 	public abstract void selecionar(ActionEvent event);
 
+	public abstract void cadastrar();
+
 	public abstract void salvar();
 
-	public abstract void remover();
+	public abstract void remover(ActionEvent event);
 
 	public final void redirect(String pagina) {
 		try {
 			FacesContext.getCurrentInstance().getExternalContext().redirect(pagina);
 		} catch (Exception ex) {
 			ex.printStackTrace();
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro!",
-					"Ocorreu uma falha no redirecionamento da página. Consulte o suporte da ferramenta."));
+			showErrorMessage("Ocorreu uma falha no redirecionamento da página. Consulte o suporte da ferramenta.");
 		}
 	}
 
-	public final void redirect(String pagina, FacesMessage mensagem) {
+	public final void redirect(String pagina, String mensagem) {
 		try {
 			FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
-			FacesContext.getCurrentInstance().addMessage(null, mensagem);
+			showSuccessMessage(mensagem);
 			FacesContext.getCurrentInstance().getExternalContext().redirect(pagina);
 		} catch (Exception ex) {
 			ex.printStackTrace();
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro!",
-					"Ocorreu uma falha no redirecionamento da página. Consulte o suporte da ferramenta."));
+			showErrorMessage("Ocorreu uma falha no redirecionamento da página. Consulte o suporte da ferramenta.");
 		}
+	}
+
+	public final void showErrorMessage(String mensagem) {
+		FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_ERROR, "Erro!", mensagem));
+	}
+
+	public final void showSuccessMessage(String mensagem) {
+		FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "Sucesso!", mensagem));
+
+	}
+
+	public final void showWarningMessage(String mensagem) {
+		FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+		FacesContext.getCurrentInstance().addMessage(null,
+				new FacesMessage(FacesMessage.SEVERITY_WARN, "Atenção!", mensagem));
+
 	}
 
 	public final void showStatusDialog() {
@@ -52,10 +69,6 @@ public abstract class AbstractBean implements Serializable {
 
 	public final void hideStatusDialog() {
 		PrimeFaces.current().executeScript("PF('statusDialog').hide();");
-	}
-
-	public final Executor getExecutor() {
-		return MoreExecutors.directExecutor();
 	}
 
 }
